3da6fc17ea2541f4463e6a202f38f49278c297b4
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-24 23:21:18 +0530

    Use of Data structure with  Linked list

---
+++ src/com/bridgelabz/LinkedList.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+import com.bridgelabz.Nodes.Node;
+
+public class LinkedList {
+	    Node head;
+	    Node tail;
+
+	    public void add(int i) {
+	        
+  }
+}   
---
+++ src/com/bridgelabz/Nodes.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import com.bridgelabz.Nodes.Node;
+
+public class Nodes {
+	public class Node {
+	    int data;
+	    Node next;
+
+	    // Parameter Constructor
+	    Node(int data) {
+	        this.data = data;
+	        this.next = null;
+	  }
+	}
+}
