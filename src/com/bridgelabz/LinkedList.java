04bf14d066e66280e66fe4c63744ce2184a7f3ae
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-24 23:29:47 +0530

    Added: Adding Nodes

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -5,7 +5,28 @@ public class LinkedList {
 	    Node head;
 	    Node tail;
 
-	    public void add(int i) {
-	        
-  }
-}   
+	    public void add(int data) {
+	        Node newNode = new Node(data);
+
+	        if (head == null) {
+	            //   System.out.println("Linked list is Empty");
+	            head = newNode;
+	            tail = newNode;
+	        } else {
+	            newNode.next = head;
+	            head = newNode;
+	        }
+	    }
+
+	    public void showList() {
+	        if (head == null) {
+	            System.out.println("Linked list is Empty.");
+	        } else {
+	            Node temp = head;
+	            while (temp != null) {
+	                System.out.print(temp.data + "->");
+	                temp = temp.next;
+	          }
+	      }
+	 }
+}
